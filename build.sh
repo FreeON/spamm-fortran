b0da8e10ae07842ffdc53674848688f4189e5b58
Nicolas Bock <nicolasbock@freeon.org>
2015-08-28 13:38:42 -0600

    Add travis script.

--- build.sh
+++ build.sh
@@ -8,9 +8,13 @@ mkdir ${INSTALL_DIR} || exit
 
 cd ${BUILD_DIR}
 cmake .. \
+  -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug} \
+  -DCMAKE_C_COMPILER=${CC:=gcc} \
+  -DCMAKE_Fortran_COMPILER=${FC:=gfortran} \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+  -DCMAKE_VERBOSE_MAKEFILE=yes \
   -DSPAMM_TESTING=yes \
   || exit
 make || exit
-make test || exit
+ctest --verbose || exit
 make install || exit
