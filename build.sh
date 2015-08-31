a82c522137a5d0a0f73a3614be5ca0a8bbe756cd
Nicolas Bock <nicolasbock@freeon.org>
2015-08-31 13:15:27 -0600

    Add more options to build.sh.

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ cmake .. \
   -DCMAKE_Fortran_COMPILER=${FC:=gfortran} \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
   -DCMAKE_VERBOSE_MAKEFILE=yes \
+  -DSPAMM_BLOCK_SIZE=${SPAMM_BLOCK_SIZE:=4} \
+  -DSPAMM_CHUNK_SIZE=${SPAMM_CHUNK_SIZE:=32} \
   -DSPAMM_TESTING=yes \
   || exit
 make || exit
